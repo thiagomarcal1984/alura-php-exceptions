996257faf8a6489f38b1663113867b88d709a8af
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-16 10:23:48 -0300

    Anotações para uso nas IDE.

---
+++ excecao.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @throws Exception
+ * As IDE mostram esta anotação para o desenvolvedor e aumentam a 
+ * compreensão do código.
+ */
+function funcaoQueLancaExcecao() 
+{ }
+
+function outraFuncao()
+{
+    funcaoQueLancaExcecao();
+}
