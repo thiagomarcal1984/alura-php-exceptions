0697ef35fd06889b2bc720838abd7d9dee38361e
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-19 22:13:49 -0300

    Configurações de erros.

--- erros.php
+++ erros.php
@@ -1,5 +1,30 @@
 <?php
 
+// Meios de forçar a exibição dos vários tipos de erro em PHP:
+// use as constantes de erro do PHP:
+// https://www.php.net/manual/en/errorfunc.constants.php
+
+// Para mostrar o caminho do php.ini, digite o comando php --ini!
+// Faça isso para alterar o parâmetro de error_reporting no php.ini.
+
+// Outra forma de alterar o parâmetro de error_reporting é o seguinte comando:
+ini_set('error_reporting', E_ALL); // Põe na configuração o parâmetro E_ALL.
+
+// O PHP também tem uma função própria chamada error_reporting(E_ALL);
+// error_reporting(E_ALL); // O parâmetro é o conjunto dos erros reportáveis.
+
+ini_set('display_errors', 0); 
+// O 2o parâmetro diz se os erros vão ser exibidos. True exibe, false não.
+// Se o parâmetro log_errors estiver ligado, os erros vão para o log padrão,
+// que é a saída padrão (tela), mesmo com o display_errors=False.
+
+ini_set('log_errors', 0);
+// O 2o parâmetro diz se haverá log. True registra, false não.
+
+// ini_set('error_log', 'log.txt');
+// O 2o parâmetro diz o caminho para o log.
+
+
 define('CONSTANTE1', 'valor_da_constante'); // Um jeito de definir constante.
 const CONSTANTE2 = 'Valor da Constante'; // Outro jeito de definir constante.
 
