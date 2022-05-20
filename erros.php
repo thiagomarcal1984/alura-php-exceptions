5dea6673bb82287575afcb6ee9038b84646ad507
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-19 22:36:51 -0300

    Tratamento de erros.

--- erros.php
+++ erros.php
@@ -18,11 +18,29 @@ ini_set('display_errors', 0);
 // Se o parâmetro log_errors estiver ligado, os erros vão para o log padrão,
 // que é a saída padrão (tela), mesmo com o display_errors=False.
 
-ini_set('log_errors', 0);
+ini_set('log_errors', 1);
 // O 2o parâmetro diz se haverá log. True registra, false não.
 
 // ini_set('error_log', 'log.txt');
 // O 2o parâmetro diz o caminho para o log.
+
+// https://www.php.net/manual/pt_BR/function.set-error-handler.php
+// set_error_handler precisa de uma função anônima que represente um handler.
+// handlers tem os seguintes parâmetros: $errno, $errstr, $errfile, $errline.
+set_error_handler(function($errno, $errstr, $errfile, $errline){
+    // var_dump($errno, $errstr, $errfile, $errline);
+    switch ($errno){
+        case E_WARNING:
+            echo "Aviso: isso é perigoso." . PHP_EOL;
+            break;
+        case E_NOTICE:
+            echo "Melhor não fazer isso." . PHP_EOL;
+            break;
+    }
+
+    // Este retorno serve para evitar o Error Handler padrão do PHP.
+    return true; 
+});
 
 
 define('CONSTANTE1', 'valor_da_constante'); // Um jeito de definir constante.
@@ -38,12 +56,16 @@ const CONSTANTE2 = 'Valor da Constante'; // Outro jeito de definir constante.
 
 // E_NOTICE não causa problemas na execução.
 echo "Acessando uma variável não declarada:";
-echo $variavel; // Erro do tipo E_NOTICE (no PHP 7).
+// O @ é um operador de supressão de erro! A próxima linha não aparecerá.
+// Usar o operador de supressão de erro NÃO é uma boa prática. Tire o @!
+echo @$variavel; // Erro do tipo E_NOTICE (no PHP 7).
 
 echo PHP_EOL . "Acessando um índice inexistente em um array (também inexistente):";
 echo $array[12]; // Outro erro do tipo E_NOTICE (no PHP 7).
 
 // E_WARNING causará problemas na execução em versões futuras do PHP.
 echo PHP_EOL . "Acessando a constante CONSTANTE: ";
-echo CONSTANTE3; // Erro do tipo E_WARNING (no PHP 7).
+// echo CONSTANTE3; // Erro do tipo E_WARNING (no PHP 7).
 // Até o PHP 7, o resultado do comando acima seria imprimir o texto CONSTANTE3.
+echo @CONSTANTE3; // O @ é um operador de supressão de erro!
+echo "Fim do programa.";
